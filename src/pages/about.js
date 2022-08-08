b0b407612c7d924e4d1789b3d8c41aabe8f3c165
NRotondaro <niksonrotondaro1@gmail.com>
2022-08-08 22:42:19 +0000

    added first pages and styled components plugin

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const About = () => {
+  return (
+    <Layout>
+      <div className="about-heading">
+        <h2>Hello from about page</h2>
+      </div>
+    </Layout>
+  )
+}
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <div className="home-heading">
+        <h1>homepage</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, sunt quo
+          iure modi expedita neque ab impedit pariatur deleniti animi maiores.
+          Iste ipsa accusamus officia quae quia dolor consequuntur aperiam?
+        </p>
+      </div>
+    </Layout>
+  )
 }
