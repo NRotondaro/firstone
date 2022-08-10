5f92e890d9fad3f2c10028ef8ddab6d4f20e1257
NRotondaro <niksonrotondaro1@gmail.com>
2022-08-10 19:23:25 +0000

    fetching data using queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,31 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipes site",
+    author: "@nikson",
+    person: {
+      name: "nikson",
+      age: 21,
+    },
+    simpleData: ["item1", "item2"],
+    complexData: [
+      { name: "nikson", age: 21 },
+      { name: "rotondaro", age: 22 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
   ],
 }
