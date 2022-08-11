9f2bae8ed32e3932b70a1f67e505616f2cb2ca2e
NRotondaro <niksonrotondaro1@gmail.com>
2022-08-11 23:59:54 +0000

    recipes created

---
+++ src/components/AllRecipes.js
@@ -0,0 +1,14 @@
+import React from "react"
+import TagsList from "./TagsList"
+import RecipesList from "./RecipesList"
+
+const AllRecipes = () => {
+  return (
+    <div>
+      <TagsList />
+      <RecipesList />
+    </div>
+  )
+}
+
+export default AllRecipes
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
-import Gallery from "../examples/Gallery"
+import AllRecipes from "../components/AllRecipes"
 
 export default function Home() {
   return (
@@ -23,7 +23,7 @@ export default function Home() {
           </div>
         </header>
       </main>
-      <Gallery />
+      <AllRecipes />
     </Layout>
   )
 }
