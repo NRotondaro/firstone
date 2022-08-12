9ecab31852e82477f4d02afe3536c1627f4a2ce6
NRotondaro <niksonrotondaro1@gmail.com>
2022-08-12 00:26:41 +0000

    feat(recipes-list): added the link and gatsby image with details

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from "react"
+import { Link } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const RecipesList = () => {
+const RecipesList = ({ recipes = [] }) => {
   return (
-    <div>RecipesList</div>
+    <div className="recipes-list">
+      {recipes.map(recipe => {
+        const { id, title, image, prepTime, cookTime } = recipe
+        const pathToImage = getImage(image)
+        return (
+          <Link to={`/${title}`} key={id}>
+            <GatsbyImage
+              image={pathToImage}
+              className="recipe-img"
+              alt={title}
+            />
+            <h5>{title}</h5>
+            <p>
+              Prep: {prepTime} min | Cook: {cookTime} min
+            </p>
+          </Link>
+        )
+      })}
+    </div>
   )
 }
 
